6e50d0975585b2346ce50590dc9325c06e4fc230
yassineATT <yassineatta@hotmail.com>
2023-04-22 14:29:33 +0200

    [ADD] Recover product data

--- src/services/shop/shop.context.js
+++ src/services/shop/shop.context.js
@@ -1,12 +1,14 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import { DataStore } from "aws-amplify";
-import { Shop, Product } from "../../models";
+import { Shop, Product, ProductDetail } from "../../models";
 
 export const ShopContext = createContext();
 
 export const ShopProvider = ({ children }) => {
   const [shops, setShops] = useState([]);
   const [products, setProducts] = useState([]);
+  const [productsDetails, setProductsDetails] = useState([]);
+  const [selectedColor, setSelectedColor] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -34,12 +36,49 @@ export const ShopProvider = ({ children }) => {
     }
   };
 
+  const getProductsDetails = async (productId) => {
+    setLoading(true);
+    try {
+      const fetchedProductsDetails = await DataStore.query(ProductDetail);
+      const filteredProductsDetails = fetchedProductsDetails.filter(
+        (p) => p.productID === productId
+      );
+      setProductsDetails(filteredProductsDetails);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getUniqueColors = (productDetails) => {
+    return productDetails.reduce((uniqueColors, productDetail) => {
+      if (!uniqueColors.includes(productDetail.color)) {
+        uniqueColors.push(productDetail.color);
+      }
+      return uniqueColors;
+    }, []);
+  };
+
+  const handleColorChange = (color) => {
+    if (selectedColor === color) {
+      setSelectedColor(null);
+    } else {
+      setSelectedColor(color);
+    }
+  };
+
   const shopContext = {
     shops,
     products,
+    productsDetails,
+    selectedColor,
     loading,
     getShops,
     getProducts,
+    getProductsDetails,
+    getUniqueColors,
+    handleColorChange,
   };
 
   return (
