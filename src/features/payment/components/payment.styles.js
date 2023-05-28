7f24eb4aba1e01e189fe14c18e1ec05ce9de1ce3
yassineATT <yassineatta@hotmail.com>
2023-05-28 10:37:59 +0200

    [ADD] Payment screen

---
+++ src/features/payment/components/payment.styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+export const Header = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 16px;
+`;
+
+export const CardLogo = styled.Image`
+  width: 200px;
+  height: 200px;
+  align-self: center;
+  margin-bottom: 16px;
+`;
+
+export const ChoiceContainer = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const ChoiceText = styled.Text`
+  font-size: 16px;
+  margin-right: 8px;
+`;
+
+export const ChoiceSelected = styled.Text`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.text.blue};
+  font-weight: bold;
+  margin-right: 8px;
+`;
+
+export const ChoiceUnselected = styled.Text`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.text.secondary};
+  margin-right: 8px;
+  opacity: 0.5;
+`;
+
+export const CardForm = styled.View`
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+  padding: 16px;
+  margin-bottom: 16px;
+`;
+
+export const PaymentButton = styled.TouchableOpacity`
+  background-color: ${(props) => props.theme.colors.ui.primary};
+  padding: 16px;
+  align-items: center;
+`;
+
+export const PaymentButtonText = styled.Text`
+  color: ${(props) => props.theme.colors.text.primary};
+  font-size: 16px;
+`;
---
+++ src/features/payment/screens/payment.screen.js
@@ -0,0 +1,72 @@
+import React, { useState, useContext } from "react";
+import {
+  Container,
+  Header,
+  CardLogo,
+  ChoiceContainer,
+  ChoiceText,
+  ChoiceSelected,
+  ChoiceUnselected,
+  CardForm,
+  PaymentButton,
+  PaymentButtonText,
+} from "../components/payment.styles";
+import { TouchableOpacity, Text } from "react-native";
+import { PaymentContext } from "../../../services/payment/payment.context";
+import { useRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+export const PaymentScreen = () => {
+  const navigation = useNavigation();
+  const [paymentMethod, setPaymentMethod] = useState("card");
+  const { handlePayment } = useContext(PaymentContext);
+
+  const route = useRoute();
+  const selectedBaskets = route.params?.baskets || [];
+
+  const handlePaymentPress = () => {
+    handlePayment(selectedBaskets);
+  };
+
+  return (
+    <Container>
+      <Header>Paiement</Header>
+      <Ionicons
+        onPress={() => navigation.goBack()}
+        name="arrow-back-outline"
+        size={30}
+      />
+      <CardLogo
+        source={{
+          uri: "https://www.iconarchive.com/download/i90679/icons8/windows-8/Finance-Bank-Cards.512.png",
+        }}
+      />
+      <ChoiceContainer>
+        <ChoiceText>Choix de Paiement:</ChoiceText>
+        <TouchableOpacity onPress={() => setPaymentMethod("card")}>
+          {paymentMethod === "card" ? (
+            <ChoiceSelected>Carte Bancaire</ChoiceSelected>
+          ) : (
+            <ChoiceUnselected>Carte Bancaire</ChoiceUnselected>
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setPaymentMethod("stripe")}>
+          {paymentMethod === "stripe" ? (
+            <ChoiceSelected>Stripe</ChoiceSelected>
+          ) : (
+            <ChoiceUnselected>Stripe</ChoiceUnselected>
+          )}
+        </TouchableOpacity>
+      </ChoiceContainer>
+      {paymentMethod === "card" && (
+        <CardForm>
+          <Text>Formulaire de carte bancaire</Text>
+        </CardForm>
+      )}
+      <PaymentButton onPress={handlePaymentPress}>
+        <PaymentButtonText>Payer</PaymentButtonText>
+      </PaymentButton>
+    </Container>
+  );
+};
