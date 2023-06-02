c3be7608d91ac1136a87abc1278c61dc92bf653b
yassineATT <yassineatta@hotmail.com>
2023-06-02 22:37:05 +0200

    [ADD] Content on settingpPage

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -2,13 +2,27 @@ import React, { useContext } from "react";
 import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthenticationContext } from "../../../services/authentification/auth.context";
+import {
+  SignOutButton,
+  ImageConstruction,
+  Bienvenue,
+} from "../components/profile.styles";
 
 export const SettingScreen = () => {
-  const { onSignOut } = useContext(AuthenticationContext);
+  const { onSignOut, user } = useContext(AuthenticationContext);
+  const constructionGif = "../../../../assets/construction.gif";
 
   return (
     <SafeAreaView>
-      <Text onPress={() => onSignOut()}>Sign Out</Text>
+      <Bienvenue>Bienvenue {user.attributes.name} !</Bienvenue>
+      <ImageConstruction
+        source={require(constructionGif)}
+        mode="contain"
+        alt="construction"
+      />
+      <SignOutButton onPress={() => onSignOut()}>
+        <Text>Se déconnecter</Text>
+      </SignOutButton>
     </SafeAreaView>
   );
 };
